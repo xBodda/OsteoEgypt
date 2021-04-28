119656684e8ecb678048e81ba96e8feabd85dece
Abdelrahman Sayed <61325578+xBodda@users.noreply.github.com>
2021-04-28 22:02:18 +0200

    add css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="{{ asset('css/master.css') }}" rel="stylesheet">
     <title>@yield('title')</title>
 </head>
 
