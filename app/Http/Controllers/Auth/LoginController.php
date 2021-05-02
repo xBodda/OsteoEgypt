eaacfe876d3b1c2a36dfe1c4d35be3ce9d3c218e
xTrimy <14347568+xTrimy@users.noreply.github.com>
2021-05-02 07:51:19 +0200

    Add Auth controllers

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function index(){
+        if(Auth::check()){
+            return redirect()->route('home');
+        }
+        return view('pages.login_demo');
+    }
+
+    public function login(Request $request)
+    {
+
+        $credentials  = $request->only(['email','password']);
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->route('home');
+        }
+        return back()->withInput()->with('status', 'Invalid login details!');
+    }
+}
