8c2c457d9a21186d271daabc73cdd53718bd75f4
xTrimy <14347568+xTrimy@users.noreply.github.com>
2021-05-02 07:55:29 +0200

    Removed cached/ignored vendor

---
+++ resources/views/pages/login_demo.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('title')
+    Login
+@endsection
+
+@section('contents')
+    @if(session('status'))
+        <div class="alert alert-fail">
+            {{ session('status') }}
+        </div>
+    @endif
+    <form action="{{ route('login') }}" method="POST">
+        @csrf
+        <input type="email" name="email" value="{{ old('email') }}">
+        <input type="password" name="password">
+        <input type="submit">
+    </form>
+
+@endsection
