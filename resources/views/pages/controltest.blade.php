3633b7db3ebc3ba20c7a948710567a6aadc0b2dc
xTrimy <14347568+xTrimy@users.noreply.github.com>
2021-06-07 14:14:39 +0200

    Add Control Panel Layout

---
+++ resources/views/pages/controltest.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.control')
+
+@section('title')
+Test
+@endsection
+
+@section('content')
+<div class="w-full bg-white relative p-3 rounded">
+    <h1 class="mb-2">Test</h1>
+    <h2 class="mb-4">Test</h2>
+    <p class="mb-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos libero eum dolores! Rem, error eveniet dolorum officia rerum id iure illum tempora vero! Numquam eaque nostrum repellat, recusandae vero laudantium.</p>
+    <hr>
+</div>
+@endsection
