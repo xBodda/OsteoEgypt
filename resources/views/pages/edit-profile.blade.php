3c1c398afed7931d52de0dad9d3664a2570c5843
Abdelrahman Sayed <61325578+xBodda@users.noreply.github.com>
2021-06-17 04:49:43 +0200

    edit profile

--- resources/views/pages/edit-profile.blade.php
+++ resources/views/pages/edit-profile.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.profile')
+@extends('layouts.app')
 
 @section('title')
     Edit Profile
@@ -10,10 +10,45 @@
 </div>
 @endsection
 
-@section('profile-data')
-    
-@endsection
+@section('contents')
+    <h1 class="text-3xl text-blueGray-dark font-bold mb-4">Edit Profile</h1>
+    <div class="flex">
+        <div class="w-2/6 bg-white shadow mr-7">
+            <div class="p-6 text-white bg-blueGray cursor-pointer text-center font-bold">User Info</div>
+            <div class="p-6 text-blueGray bg-white cursor-pointer text-center font-bold border border-solid hover:bg-blueGray hover:text-white transition-all">Privacy</div>
+            <div class="p-6 text-blueGray bg-white cursor-pointer text-center font-bold border border-solid hover:bg-blueGray hover:text-white transition-all">Payment</div>
+            <div class="p-6 text-blueGray bg-white cursor-pointer text-center font-bold border border-solid hover:bg-blueGray hover:text-white transition-all">Settings</div>
+        </div>
+        <div class="w-4/6 bg-white p-6 shadow">
+            <div class="flex">
+                <div class="h-36 w-36 mr-10">
+                    <img class="w-full h-full object-cover" src="{{ asset('assets/image/profile-img.jpg') }}"/>
+                </div>
+                <div>
+                    <h1 class="w-full">Hagar Mohamed Refaat</h1>
+                    <p class="mb-6">Cairo, Egypt</p>
+                    <div class="flex-1 text-center rounded-3xl bg-chillBlue text-white px-2 py-2 text-lg font-bold cursor-pointer">Edit Profile Picture</div>
+                </div>
+            </div>
+            <form action="{{ route('edit-profile') }}" method="GET">
+                @csrf
 
-@section('profile-title')
-    Edit Profile
+                <div class="error">
+                    <div class="flex justify-between text-red-500 ">
+                        <label for=""></label>
+                        <p></p>
+                    </div>
+                    <input class="border border-solid text-gray-900  border-blueGray bg-blueGray-lightest "  type="" id="" name="" value="" placeholder="">
+                </div>
+                
+                <x-input type="email"  name="email" message="Email Address" placeholder="Your Email Address"/>
+                <x-input type="password" name="password"  message="Password" placeholder="Password" value="" />
+                <x-input type="password" name="password_confirmation"  message="Confirm Password" placeholder="Confirm Password" value="" />
+
+                <div class="submit-button-container">
+                    <input type="submit" class="submit-button" value="Sign Up">
+                </div>
+            </form>
+        </div>
+    </div>
 @endsection
