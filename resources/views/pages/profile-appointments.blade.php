90c0ff2ddcd6701b8e4c5ecfcda9b2bfe022a1e6
Abdelrahman Sayed <61325578+xBodda@users.noreply.github.com>
2021-06-20 06:06:11 +0200

    some changes

--- resources/views/pages/profile-appointments.blade.php
+++ resources/views/pages/profile-appointments.blade.php
@@ -31,7 +31,7 @@
 <h1 class="text-2xl font-extrabold text-blueGray-dark">
     Upcoming
 </h1>
-<div class="flex w-full h-44 border border-solid border-blueGray-light mt-3 relative">
+<div class="flex w-full h-44 border border-solid border-blueGray-light mt-3 relative mb-8 bg-white">
     <div class="flex-initial w-2/12 h-full bg-chillBlue text-white flex flex-col justify-center items-center">
         <h1 class="text-4xl uppercase">Jan 06</h1>
         <p class="text-lg">08:00 PM</p>
@@ -75,7 +75,46 @@
     </div>
     
 </div>
-    
+<h1 class="text-2xl font-extrabold text-blueGray-dark">
+    History
+</h1>
+<div class="flex w-full h-44 border border-solid border-blueGray-light mt-3 relative bg-white">
+
+    <div class="w-8/12 p-6 flex flex-col justify-between ">
+        <div class="flex">
+            <div class="flex-1 flex text-lg">
+                <div class="text-gray-400 mr-8 uppercase w-32">Location</div>
+                <div class="text-blueGray-dark font-bold flex-initial">Masr-elgedida-cairo</div>
+            </div>
+            <div class="flex-1 flex text-lg">
+                <div class="text-gray-400 mr-4 uppercase w-32">Date</div>
+                <div class="text-blueGray-dark font-bold">xxxxxxxxxxx</div>
+            </div>
+        </div>
+        <div class="flex">
+            <div class="flex-1 flex text-lg">
+                <div class="text-gray-400 mr-8 uppercase w-32">Visit type</div>
+                <div class="text-blueGray-dark font-bold">xxxxx</div>
+            </div>
+            <div class="flex-1 flex text-lg">
+                <div class="text-gray-400 mr-4 uppercase w-32">Time</div>
+                <div class="text-blueGray-dark font-bold">xxxxx</div>
+            </div>
+        </div>
+        <div class="flex">
+            <div class="flex-1 flex text-lg">
+                <div class="text-gray-400 mr-8 uppercase w-32">Doctor</div>
+                <div class="text-blueGray-dark font-bold">xxxx</div>
+            </div>
+        </div>
+        
+    </div>
+    <div class="absolute flex h-10 bottom-0 right-0">
+        <button class="w-52 flex items-center justify-center px-8 py-3 border-none bg-chillBlue text-white cursor-pointer font-bold hover:bg-chillBlue-dark">
+             Book Again
+        </button>
+    </div>
+</div>
 @endsection
 
 @section('profile-title')
