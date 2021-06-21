c57bfe0f417a679c9d3f0cadda52f4ba84f72204
Muhammad Tarek <68518117+MuhammadTarekk@users.noreply.github.com>
2021-06-21 09:31:53 +0200

    FAQ Final

--- resources/views/pages/faq.blade.php
+++ resources/views/pages/faq.blade.php
@@ -24,34 +24,91 @@
         </div>
 <div class="flex flex-col w-3/4">
         <div class="m-2">
-        <h1 class="">FAQ</h1>
+        <h1 class="mb-4 text-chillBlue">Frequently Asked Questions</h1>
         </div>
-        <div class="question p-1 border-none rounded-lg bg-blue-600">
-                <button class=""></button>
-                <span class="answer p-2 border-none rounded-lg bg-green-200 hidden">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem nobis ex facere neque ab molestias fugit atque, cum id animi debitis velit tempore et illum repellendus iusto minima consectetur error.</span>
+        <div class="question1 flex flex-col p-1 rounded-lg bg-white p-4 pl-0.5 mb-4">
+            <div class="flex flex-row ">
+                <button class="b1 fas fa-plus-circle fa-lg w-1/12 border-none bg-white text-chillBlue" ></button>
+                <h2 class="text-gray-400 ">What Is OsteoPathy?</h2>
+            </div>
+                <p class="answer1 p-2 ml-8 border-none rounded-lg hidden text-gray-400">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem nobis ex facere neque ab molestias fugit atque, cum id animi debitis velit tempore et illum repellendus iusto minima consectetur error.</p>
+        </div>
+        <div class="question2 flex flex-col p-1 rounded-lg bg-white p-4 pl-0.5 mb-4">
+            <div class="flex flex-row ">
+                <button class="b2 fas fa-plus-circle fa-lg w-1/12 border-none bg-white text-chillBlue" ></button>
+                <h2 class="text-gray-400 ">What Is OsteoPathy?</h2>
+            </div>
+                <p class="answer2 p-2 ml-8 border-none rounded-lg hidden text-gray-400">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem nobis ex facere neque ab molestias fugit atque, cum id animi debitis velit tempore et illum repellendus iusto minima consectetur error.</p>
+        </div>
+        <div class="question3 flex flex-col p-1 rounded-lg bg-white p-4 pl-0.5 mb-4">
+            <div class="flex flex-row ">
+                <button class="b3 fas fa-plus-circle fa-lg w-1/12 border-none bg-white text-chillBlue" ></button>
+                <h2 class="text-gray-400 ">What Is OsteoPathy?</h2>
+            </div>
+                <p class="answer3 p-2 ml-8 border-none rounded-lg hidden text-gray-400">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem nobis ex facere neque ab molestias fugit atque, cum id animi debitis velit tempore et illum repellendus iusto minima consectetur error.</p>
+        </div>
+        <div class="question4 flex flex-col p-1 rounded-lg bg-white p-4 pl-0.5 mb-4">
+            <div class="flex flex-row ">
+                <button class="b4 fas fa-plus-circle fa-lg w-1/12 border-none bg-white text-chillBlue" ></button>
+                <h2 class="text-gray-400 ">What Is OsteoPathy?</h2>
+            </div>
+                <p class="answer4 p-2 ml-8 border-none rounded-lg hidden text-gray-400">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem nobis ex facere neque ab molestias fugit atque, cum id animi debitis velit tempore et illum repellendus iusto minima consectetur error.</p>
+        </div>
+        <div class="question5 flex flex-col p-1 rounded-lg bg-white p-4 pl-0.5 mb-4">
+            <div class="flex flex-row ">
+                <button class="b5 fas fa-plus-circle fa-lg w-1/12 border-none bg-white text-chillBlue" ></button>
+                <h2 class="text-gray-400 ">What Is OsteoPathy?</h2>
+            </div>
+                <p class="answer5 p-2 ml-8 border-none rounded-lg hidden text-gray-400">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem nobis ex facere neque ab molestias fugit atque, cum id animi debitis velit tempore et illum repellendus iusto minima consectetur error.</p>
         </div>
     </div>
 </div>
     <script>
-        // var x = document.getElementsByClassName("answer");
-        // if (window.getComputedStyle(x).display === "none"){
-        //     $(document).ready(function() {
-        //             $(".question").click(function(){
-        //             $(".answer").slideUp();
-        //             });
-        //     });
-        // }
-        // else{
-        //     $(document).ready(function() {
-        //             $(".question").click(function(){
-        //             $(".answer").slideDown();
-        //             });
-        //     });
-        // }
         $(document).ready(function() {
-                    $(".question").click(function(){
-                    $(".answer").slideToggle();
+                    $(".question1").click(function(){
+                    $(".answer1").slideToggle();
                     });
+                     $('.question1').click(function() {
+                          $('.b1').toggleClass('fas fa-plus-circle fas fa-minus-circle');
+                     });
             });
+
+            $(document).ready(function() {
+                    $(".question2").click(function(){
+                    $(".answer2").slideToggle();
+                    });
+                    $('.question2').click(function() {
+                          $('.b2').toggleClass('fas fa-plus-circle fas fa-minus-circle');
+                     });
+
+            });
+            $(document).ready(function() {
+                    $(".question3").click(function(){
+                    $(".answer3").slideToggle();
+                    });
+                    $('.question3').click(function() {
+                          $('.b3').toggleClass('fas fa-plus-circle fas fa-minus-circle');
+                     });
+
+            });
+            $(document).ready(function() {
+                    $(".question4").click(function(){
+                    $(".answer4").slideToggle();
+                    });
+                    $('.question4').click(function() {
+                          $('.b4').toggleClass('fas fa-plus-circle fas fa-minus-circle');
+                     });
+
+            });
+            $(document).ready(function() {
+                    $(".question5").click(function(){
+                    $(".answer5").slideToggle();
+                    });
+                    $('.question5').click(function() {
+                          $('.b5').toggleClass('fas fa-plus-circle fas fa-minus-circle');
+                     });
+
+            });
+
     </script>
 @endsection()
